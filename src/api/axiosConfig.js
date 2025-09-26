1d190349aa3e9194c2b719b029e7ad9c7d17d291
somenamejust <somenamejust@gmail.com>
2025-09-26 15:56:15 +0300

    Added REACT_APP_API_URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Создаем экземпляр axios с базовой конфигурацией
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // Указываем базовый URL вашего API
-  withCredentials: true // 👈 САМАЯ ВАЖНАЯ СТРОКА
+  // Use the environment variable for production, fallback to localhost for development
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  withCredentials: true
 });
 
 export default instance;
--- src/pages/LobbyIn.jsx
+++ src/pages/LobbyIn.jsx
@@ -80,7 +80,7 @@ export default function LobbyIn() {
   useEffect(() => {
     if (!lobbyId || !user) return; // Ждём, пока загрузится и лобби, и юзер
 
-    const socket = io("http://localhost:5000");
+    const socket = io("http://164.92.250.91:5000");
     socketRef.current = socket;
 
     const handleConnect = () => {
